85550e3996629443dd54533847613a59cb71bd0b
Pavel Mikhalchuk <pmikhalchuk@PMMacBook-2.local>
2020-02-01 12:06:27 +0300

    initial commit

---
+++ pkg/apis/mikhalchuk/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the mikhalchuk v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=mikhalchuk
+package v1alpha1
---
+++ pkg/apis/mikhalchuk/v1alpha1/namespace_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NamespaceSpec defines the desired state of Namespace
+type NamespaceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// NamespaceStatus defines the observed state of Namespace
+type NamespaceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Namespace is the Schema for the namespaces API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=namespaces,scope=Namespaced
+type Namespace struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NamespaceSpec   `json:"spec,omitempty"`
+	Status NamespaceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NamespaceList contains a list of Namespace
+type NamespaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Namespace `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Namespace{}, &NamespaceList{})
+}
